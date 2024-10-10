28f95c5d90065ee8e54b8093f488caa007a3bf8f
Muraina020 <glonal-murainaboluwatife@gmail.com>
2024-10-10 02:32:55 +0100

    greenhouse project

--- app/contact/Social.jsx
+++ app/contact/Social.jsx
@@ -8,13 +8,13 @@ import styles from "./contact.module.css"
 const Social = () => {
   return (
     <div className={styles.contactWrapper}>
-    <a href="murainaboluwatife@gmail.com" className={styles.contactOption} target='_blank' rel='noopener noreferrer'>
+    <a href="adelineniwenshuti@gmail.com" className={styles.contactOption} target='_blank' rel='noopener noreferrer'>
       < MdOutlineEmail className={styles.contactIcon}/>
     </a>
-    <a href="https://www.instagram.com/muraina020" className={styles.contactOption} target='_blank' rel='noopener noreferrer'>
+    <a href="https://www.instagram.com/derynegreenhouse1" className={styles.contactOption} target='_blank' rel='noopener noreferrer'>
       <RiMessengerLine className={styles.contactIcon}/>
     </a>
-    <a href="https://wa.me/+2347061048347" className={styles.contactOption} target='_blank' rel='noopener noreferrer'>
+    <a href="https://wa.me/+250787552411" className={styles.contactOption} target='_blank' rel='noopener noreferrer'>
       <BsWhatsapp  className={styles.contactIcon}/>
     </a>
     </div>
--- app/projects/data.js
+++ app/projects/data.js
@@ -1,7 +1,7 @@
 "use client"
 
 import Image1 from "../assets/proj1.jpg"
-import Image2 from "../assets/proj2.jpg"
+import Image2 from "../assets/proj-2.jpeg"
 import Image3 from "../assets/proj3.jpg"
 import Image4 from "../assets/proj4.jpg"
 import Image5 from "../assets/proj5.jpg"
