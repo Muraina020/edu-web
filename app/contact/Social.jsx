a0b7887cd1b1d1707e336e7c8e1e774b653a4780
Muraina020 <glonal-murainaboluwatife@gmail.com>
2024-10-15 23:16:50 +0100

    greenhouse

--- app/contact/Social.jsx
+++ app/contact/Social.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { BsWhatsapp } from 'react-icons/bs'
 import { FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 import styles from "./contact.module.css"
 
 
 const Social = () => {
   return (
     <div className={styles.contactWrapper}>
+      <div className={styles.social1}>
        <a href="https://wa.me/+250787552411" className={styles.contactOption} target='_blank' rel='noopener noreferrer'>
       <BsWhatsapp  className={styles.contactIcon}/>
     </a>
@@ -17,6 +20,15 @@ const Social = () => {
     <a href="https://www.instagram.com/derynegreenhouse1" className={styles.contactOption} target='_blank' rel='noopener noreferrer'>
       < FaInstagram  className={styles.contactIcon}/>
     </a>
+    </div>
+    <div className={styles.social1}>
+    <a href="https://www.facebook.com/profile.php?id=61565251337323" className={styles.contactOption} target='_blank' rel='noopener noreferrer'>
+      <FaFacebookSquare className={styles.contactIcon}/>
+    </a>
+    <a href="https://www.linkedin.com/deryne-greenhouse" className={styles.contactOption} target='_blank' rel='noopener noreferrer'>
+      < FaLinkedin  className={styles.contactIcon}/>
+    </a>
+    </div>
     </div>
   )
 }
